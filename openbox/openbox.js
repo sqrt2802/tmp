5d035a47d08117314fb459a8e422f879446e4234
sqrt2802 <sqrt2802@outlook.com>
2024-03-16 18:02:54 +0800

    Update openbox.js

--- openbox/openbox.js
+++ openbox/openbox.js
@@ -6,7 +6,7 @@ const val=parseInt(progress[pos].textContent.split(" ")[2]);if(val>=sieve){if(ou
 j++;reload();setTimeout(()=>{check();},500);}
 function reload(){if(j<all.length){if(enableQD==1){document.querySelector("#textdiv>textarea").value="!test!\n!\n\n"+all[j]+"\n"+all[j];}else{document.querySelector("#textdiv>textarea").value="!test!\n!\n\n"+all[j];}
 document.querySelector(".goBtn").click();}else{if(outmtd==2){document.querySelector("#textdiv>textarea").value=result;}
-alert("开箱完毕");if(outmtd==1){document.querySelector("textarea#result").value+="\n===结束==="}
+alert("开箱完毕");if(outmtd==1){document.querySelector("textarea#result").value+="\n===结束===\n请不要重复使用此网页!"}
 fi=1;}}
 if(outmtd==1){const NW=document.createElement("textarea");NW.id="result";document.body.appendChild(NW);NW.setAttribute("readonly",true);document.getElementsByClassName("mdframe")[0].setAttribute("style","display:none;");}else if(outmtd==2){result="开箱输出:\n\n";}
 reload();check();}
